870bc1c1222b3b26edb409c5b08069bce464467a
Jose Flores <quitzef4@gmail.com>
2018-08-27 11:35:58 -0700

    Added the init array function

---
+++ src/read_file.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+FILE * open_file(char * input_file_name)
+{
+	FILE * f_ptr;
+	if(input_file_name == NULL)
+		return NULL;
+	
+	f_ptr = fopen(input_file_name, "r");
+	
+	if(f_ptr == NULL)
+	{
+		perror("Error");
+		exit(EXIT_FAILURE);
+	}
+	
+	return f_ptr;
+}
+
+char ** init_array(int array_current_size, int word_current_size)
+{
+	int i = 0;
+	
+	char ** files_data = (char **)calloc(array_current_size , sizeof(char *));
+	if(files_data == NULL)
+	{
+		fprintf(stderr, "calloc(...) failed.");
+		exit(EXIT_FAILURE);
+	}
+	
+	
+	for(i = 0; i < array_current_size; i++)
+	{
+		files_data[i] = (char *)calloc(word_current_size, sizeof(char));
+		if(files_data[i] == NULL)
+		{
+			fprintf(stderr, "calloc(...) failed.\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+	
+	return files_data;
+}
+
+char ** read_file(FILE * f_ptr)
+{
+	int array_current_size = 100;
+	int word_current_size = 256;
+	char ** files_data = init_array(array_current_size, word_current_size);
+}
+
+
+int main(int argc, char ** argv)
+{
+	FILE * f_ptr;
+	char ** files_data;
+	if(argc != 1)
+	{
+		f_ptr = open_file(argv[1]);
+		files_data = read_file(f_ptr);
+	}
+	else
+	{
+		fprintf(stdout, "Usage : ./run input_file_name\n");
+	}
+	return 0;
+}
